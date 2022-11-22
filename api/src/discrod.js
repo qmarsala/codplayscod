f1fe67d88c74f64117a5088ee2cfb864d3a6634f
Quinten Marsala <quinten.marsala@gmail.com>
2022-11-21 21:18:29 -0800

    split out discord logic

---
+++ api/src/discrod.js
@@ -0,0 +1,20 @@
+const sendDiscordMessages = async (messages, env) => {
+    console.log("sending discord messages");
+    const msgs = messages.filter(m => m.length > 0);
+    for (var m of msgs) {
+        const response = await fetch(env.DISCORD_WEBHOOK, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "content": m })
+        });
+        if (response.status > 299) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export { sendDiscordMessages }
