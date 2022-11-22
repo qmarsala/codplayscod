8b56e82057dbdf2ba7304304ed033a0e6b0149a7
Quinten Marsala <quinten.marsala@gmail.com>
2022-11-21 23:34:19 -0800

    improve responses

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,6 @@
 import { Router } from 'itty-router';
 import { execute } from './cron';
 
-class JsonResponse extends Response {
-    constructor(body, init) {
-        const jsonBody = JSON.stringify(body);
-        init = init || {
-            headers: {
-                'content-type': 'application/json;charset=UTF-8',
-            },
-        };
-        super(jsonBody, init);
-    }
-}
-
 const logStatus = async (notification, env) => {
     const dateTime = Date.now();
     const secondsFromNow = 6000;
@@ -23,13 +11,13 @@ const router = Router();
 router.post('/', async (request, env) => {
     const notification = await request.json();
     await logStatus(notification, env);
-    return new JsonResponse({ "loggedStatus": notification.status }, { status: 202 });
+    return new Response({ "loggedStatus": notification.status }, { status: 202 });
 });
 router.get('/', async (request, env) => {
     const state = await env.DATA.get("state");
-    return new JsonResponse(state ?? {}, { status: 200 });
+    return new Response(state ?? {}, { status: 200 });
 });
-router.all('*', () => new Response('Not Found.', { status: 404 }));
+router.all('*', () => new Response({ error: "Not Found." }, { status: 404 }));
 
 export default {
     async scheduled(event, env, ctx) {
