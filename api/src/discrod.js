e6ff8d21648450e0fabcb8b4d0c892da45a1390e
Quinten Marsala <quinten.marsala@gmail.com>
2022-11-22 19:35:42 -0800

    send all messages as one message to discord

--- api/src/discrod.js
+++ api/src/discrod.js
@@ -1,21 +1,19 @@
 const sendDiscordMessages = async (messages, env) => {
     console.log("sending discord messages");
-    const msgs = messages.filter(m => m.length > 0);
-    for (var m of msgs) {
-        const response = await fetch(env.DISCORD_WEBHOOK, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "content": m })
-        });
-        if (response.status > 299) { 
-            console.log(response.statusText);
-            return false;
-        }
+    const message = messages.filter(m => m.length > 0).join(`\n`);
+    const response = await fetch(env.DISCORD_WEBHOOK, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "content": message })
+    });
+    if (response.status >= 200 && response.status < 300) {
+        return true;
     }
-    return true;
+    console.log(response.statusText);
+    return false;
 }
 
 export { sendDiscordMessages }
